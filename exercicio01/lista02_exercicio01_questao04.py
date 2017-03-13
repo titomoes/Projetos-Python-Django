aa227d5c37619cf1e1ef76f04c109bd1805368af
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 20:56:50 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao04.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+# ----------------------------------------------------------
+n1=float(input('Informe a primeira nota: '))
+n2=float(input('Informe a segunda nota: '))
+n3=float(input('Informe a terceira nota: '))
+n4=float(input('Informe a quarta nota: '))
+print('A media eh: ',((n1+n2+n3+n4)/4))
