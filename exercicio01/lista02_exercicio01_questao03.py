b0ec2050393af979034ca0ab8baf24941a291b98
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 20:49:49 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao03.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que peça dois números e imprima a soma.
+# ----------------------------------------------------------
+n1=float(input('informe o primeiro numero: '))
+n2=float(input('informe o segundo numero: '))
+print('A soma eh: ', n1+n2)
