0ffbf906645df85dc44b087322a532152078e1b5
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 20:04:17 -0400

    Lista 02, exercicio 1 *questão 7 *questão 10

---
+++ exercicio01/lista02_exercicio01_questao07.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+# ----------------------------------------------------------
+l = float(input("Digite o lado do quadrado "))
+print("Area do quadrado:", (l ** 2)*2,'m²')
---
+++ exercicio01/lista02_exercicio01_questao10.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+# ----------------------------------------------------------
+c = float(input('Temperatura em Celsius: '))
+print('A temperatura em Fahrenheit eh: ',(c+1.8)+32)
