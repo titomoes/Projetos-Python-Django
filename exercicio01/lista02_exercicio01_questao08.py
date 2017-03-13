58d036c6365d14972396ebe642c93df611d503a9
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 21:28:29 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao08.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+# ----------------------------------------------------------
+h=float(input('Quanto voce ganha por hora?: '))
+nh=float(input('Quantas horas trabalhadas no mes?: '))
+print('O salario total no mes eh: ',h*nh)
