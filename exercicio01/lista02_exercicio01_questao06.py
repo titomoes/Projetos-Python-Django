6a8b7fa0bd1e58e979d3b46ff0fdcfa5392436e2
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 21:14:17 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao06.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+# ----------------------------------------------------------
+import math
+n1=float(input('Informe o raio do circulo: '))
+print('A area do circulo eh: ',math.pi*n1**2)
