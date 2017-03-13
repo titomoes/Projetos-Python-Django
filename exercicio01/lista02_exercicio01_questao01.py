31f3473001cd88150fc96be22f389438ea77bf6b
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 20:16:47 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao01.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+# ----------------------------------------------------------
+print('Alo mundo')
