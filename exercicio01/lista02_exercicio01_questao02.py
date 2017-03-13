e263af4c45526fba7cff5de320fb01e42db73c0e
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 20:35:07 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao02.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+# ----------------------------------------------------------
+n=float(input('Informar um numero: '))
+print('O numero informado foi: ',n)
