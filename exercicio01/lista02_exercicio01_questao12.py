2c0627becd562c3e10d32648e4267e0995947208
VitorSimoes <vitor6427126@gmail.com>
2017-03-13 00:15:43 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao12.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+# ----------------------------------------------------------
+h = float(input('Digite sua altura: '))
+print('Seu peso ideal eh ', (72.7 * h) - 58)
