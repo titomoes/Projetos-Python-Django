886ba6d5ad6a16a245bfcabb0923b05cfd22ea23
VitorSimoes <vitor6427126@gmail.com>
2017-03-12 21:07:23 -0400

    Lista 02, exercicio 1

---
+++ exercicio01/lista02_exercicio01_questao05.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+#Faça um Programa que converta metros para centímetros.
+# ----------------------------------------------------------
+n1=float(input('Informe o valor em metros que deseja converter em centimetros: '))
+print('O valor em metros eh: ',n1*10**2)
