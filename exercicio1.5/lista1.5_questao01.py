07da9a7cfc9b8f852617dcfb867cd90761b5610e
VitorSimoes <vitor6427126@gmail.com>
2017-03-16 06:53:37 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao01.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que receba dois números, calcule e mostre a subtração do primeiro pelo segundo
+# ----------------------------------------------------------
+n1 = float(input('Digite o n1 '))
+n2 = float(input('Digite o n2 '))
+print('A subtração eh ', n1 - n2)
