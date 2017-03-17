4ffadf055832b05ec6ed223ecaf53e7d2421f171
VitorSimoes <vitor6427126@gmail.com>
2017-03-17 19:06:57 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao09.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que calcule e mostre a area de um trapezio
+# Sabe-se que A=((base maior +base menor)*altura)/2
+# ----------------------------------------------------------
+b_major = float(input('Digite a base maior '))
+b_less = float(input('Digite a base menor '))
+height=float(input('Digite a altura '))
+print('A area do trapezio eh ',((b_major+b_less)*height)/2)
