b1a0d26344e9f8f7e7ab40e628c4803269af9032
VitorSimoes <vitor6427126@gmail.com>
2017-03-17 18:32:07 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao05.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que receba o preço de um produto, calcule e mostre
+# o novo preço sabendo que esse sofreu um desconto de 10%
+# ----------------------------------------------------------
+price = float(input('Digite o preco '))
+print("O preco com desconto eh ", price - (price * 0.1))
