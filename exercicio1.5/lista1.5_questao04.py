c2c2e030193a9282315e4eb877acb1798545a779
VitorSimoes <vitor6427126@gmail.com>
2017-03-17 18:19:20 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao04.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que receba duas notas, calcule e mostre a média ponderada dessas notas
+# considerando peso 2 para a primeira e peso 3 para a segunda
+# ----------------------------------------------------------
+n1=float(input('Nota 1'))
+n2=float(input('Nota 2'))
+print("a media eh ",(n1*2+n2*3/(2+3)))
