74a4c2cbf982e1996d87e6736ab91fa9ec34493a
VitorSimoes <vitor6427126@gmail.com>
2017-03-17 18:58:51 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao08.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que receba o peso de uma pessoa em quilos,
+# calcule e mostre esse peso em gramas
+# ----------------------------------------------------------
+weight = float(input('Digite o peso em kg '))
+print('esse peso em gramas eh ',weight*10**3)
+
