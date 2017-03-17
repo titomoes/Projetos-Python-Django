1bcca07642f6b36f96292e20f02f8bb2473924d2
VitorSimoes <vitor6427126@gmail.com>
2017-03-17 19:12:10 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao10.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que calcule e mostre a area de um quadrado
+# Sabe-se que a area é = lado*lado
+# ----------------------------------------------------------
+l=float(input('Digite o lado do quadrado '))
+print('Area do quadrado eh ',l**2)
