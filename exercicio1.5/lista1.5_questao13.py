95d9efcd6549b366efc1fc9ada923fd335369203
VitorSimoes <vitor6427126@gmail.com>
2017-03-18 09:09:23 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao13.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que calcule e mostre a tabuada de um numero digitado pelo usuario
+# ----------------------------------------------------------
+n=int(input('Digite o numero que voce quer saber a tabuada '))
+print(n,'* 1 =',n*1)
+print(n,'* 2 =',n*2)
+print(n,'* 3 =',n*3)
+print(n,'* 4 =',n*4)
+print(n,'* 5 =',n*5)
+print(n,'* 6 =',n*6)
+print(n,'* 7 =',n*7)
+print(n,'* 8 =',n*8)
+print(n,'* 9 =',n*9)
+print(n,'* 10 =',n*10)
