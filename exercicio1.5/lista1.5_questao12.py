e89b047fba263cd0456229d65cf0a16ee00883e6
VitorSimoes <vitor6427126@gmail.com>
2017-03-18 09:01:49 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao12.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que receba um salario minimo e o valor do salario de um
+#funcionario,calcule e mostre a quantidade de salarios minimos que ganha esse funcionario
+# ----------------------------------------------------------
+smini=float(input('Digite o salario minimo '))
+salary=float(input('Digite o seu salario '))
+print('Voce recebe ',salary/smini,'salarios minimos')
