0505bf5d62eab0916c7b69c3a79a85e91ab02e5d
VitorSimoes <vitor6427126@gmail.com>
2017-03-18 08:57:10 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao11.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faca um programa que calcule e mostre a area de um losango
+# Sabe-se que a area=(diagonal maior * diagonal menor)/2
+# ----------------------------------------------------------
+dM=float(input('Digite a diagonal maior'))
+dm=float(input('Digite a diagonal menor'))
+print('A area do losango eh ',(dM*dm)/2)
