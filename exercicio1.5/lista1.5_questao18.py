adb6b3da79565dcd445f71a992bac30c68b004da
VitorSimoes <vitor6427126@gmail.com>
2017-03-22 19:42:58 -0400

    Lista 02, exercicio 1

---
+++ exercicio1.5/lista1.5_questao18.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que receba uma temperatura em Celsius, calcule e mostre
+# sabe-se que F=180(c+32)/100
+# ----------------------------------------------------------
+c=float(input('Digite a temperatura em celsius '))
+print('A temperatura em fahrenheit eh ',(9/5)*c+32)
