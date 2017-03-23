8af99393dcbf034376efdfac4152491043487b4e
VitorSimoes <vitor6427126@gmail.com>
2017-03-23 16:51:55 -0400

    Lista 02, exercicio 1

---
+++ lista1.5/lista1.5_questao23.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa que receba a medida de dois angulos de um triangulo, calcule e mostre
+# a medida do terceiro angulo.Sabe-se que a soma dos angulos de um triangulo é 180
+# ----------------------------------------------------------
+angulo1=float(input('Angulo 1'))
+angulo2=float(input('Angulo 2'))
+angulo3=180-angulo1-angulo2
+print('O terceiro angulo eh ',angulo3)
