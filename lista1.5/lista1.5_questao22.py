3b9ebca69b0c96b660f4c1d942ab42eefb3aa49f
VitorSimoes <vitor6427126@gmail.com>
2017-03-23 16:41:37 -0400

    Lista 02, exercicio 1

---
+++ lista1.5/lista1.5_questao22.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um  programa que receba o numero de lados de um poligno convexo,calcule e mostre o numero de
+# diagonais desse poligono, onde N é o numero de lados do poligono. Sabe-se que ND=N(N-3)/2.
+# ----------------------------------------------------------
+n=int(input('Numero de lados do poligono convexo '))
+print('Numero de diagonais desse poligono eh ',n*(n-3)/2)
