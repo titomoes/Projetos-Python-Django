f2f23967e61634399e9f92208e828e270f34e19a
VitorSimoes <vitor6427126@gmail.com>
2017-03-26 20:46:39 -0400

    Lista 02, exercicio 1

---
+++ lista03/lista03_exercicio01_questao03.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+# ----------------------------------------------------------
+sex = input('Digite seu sexo ')
+if (sex == 'F'):
+    print("Feminino")
+elif (sex == 'M'):
+    print('Masculino')
+else:
+    print('Sexo Inválido')
