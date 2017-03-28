4be5d0da095151b0aba0aaf0a2eb92ad874dbbd7
VitorSimoes <vitor6427126@gmail.com>
2017-03-28 15:47:12 -0400

    Lista 02, exercicio 1

---
+++ lista03/lista03_exercicio01_questao05.py
@@ -0,0 +1,23 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+# A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+# A mensagem "Reprovado", se a média for menor do que sete;
+# A mensagem "Aprovado com Distinção", se a média for igual a dez.
+# ----------------------------------------------------------
+n1 = float(input('primeira nota parcial '))
+n2 = float(input('segunda nota parcial '))
+media = (n1 + n2) / 2
+if media == 10:
+    print('Aprovado com Distinção')
+elif media >= 7:
+    print('Aprovado ')
+else:
+    print('Reprovado')
