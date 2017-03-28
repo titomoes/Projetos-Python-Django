6a159f9f0eac6d7c123f5ed0244f277041684d9b
VitorSimoes <vitor6427126@gmail.com>
2017-03-28 15:08:00 -0400

    Lista 02, exercicio 1

---
+++ lista03/lista03_exercicio01_questao04.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Vitor Simoes Azevedo	    1715310025
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+# ----------------------------------------------------------
+lyrics = input('digite a letra ')
+if lyrics == 'a' or lyrics == 'e' or lyrics == 'i' or lyrics == 'o' or lyrics == 'u':
+    print('a letra é vogal ')
+else:
+    print('é consoante')
