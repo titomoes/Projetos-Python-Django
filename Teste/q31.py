65752ca7c794017ecdda2a5212128506f1d003cf
VitorSimoes <vitor6427126@gmail.com>
2017-04-06 21:49:52 -0400

    Resolvido

--- Teste/q31.py
+++ Teste/q31.py
@@ -1,14 +1,32 @@
-total=0
-cont=0
-
+# primeiro while para controlar o registro das compras
 while True:
-    n=float(input('Produto'))
-    if n!=0:
-        total=total+n
-        cont=cont+1
+    print('Loja Tabajara')
+    cont = 1
+    total = 0
+    # segundo while para calcular cada compra
+    while True:
+        valor = float(input("Produto {}: R$ ".format(cont)))
+        cont += 1
+        total += valor
+        # se for digitado 0 encerra a compra
+        if valor == 0:
+            break
+    # calculo do Total e troco
+    print('Total: ', total)
+    din = float(input('Dinheiro :'))
+    #Condição do dinheiro for menor que o total
+    if din >= total:
+        troco = din - total
+        print('Troco: ', troco)
     else:
-        print('Produto ',cont,': ',n)
-        print('Total: ',total)
-        din=float(input('Dinheiro :'))
-        print('Troco: ',din-total)
+        print('Dinheiro insuficiente')
+        din = float(input('Dinheiro :'))
+        troco = din - total
+        print('Troco: ', troco)
+
+    reset = input('Pressione 0 para reuniciar ou 1 para encerrar: ')
+    if reset == '0':
+        continue
+    else:
+        print('Encerrando caixa')
         break
