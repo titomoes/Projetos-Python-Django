a92909a06cc1a101f904e42e22fe0e57ec2fa0a4
edsonjr <edson.limajr@gmail.com>
2017-04-11 15:54:50 -0400

    Sinta a pressao

--- Teste/lista02_questao25.py
+++ Teste/lista02_questao25.py
@@ -8,29 +8,19 @@
 # VICTOR HUGO DE OLIVEIRA CARREIRA - 1715310063
 # REINALDO VARGAS - 1715310054
 #
-# O Sr. Manoel Joaquim expandiu seus negócios para além dos negócios de 1,99 e agora possui uma loja de conveniências.
-# Faça um programa que implemente uma caixa registradora rudimentar. O programa deverá receber um número desconhecido de valores referentes aos preços das mercadorias.
-# Um valor zero deve ser informado pelo operador para indicar o final da compra.
-# O programa deve então mostrar o total da compra e perguntar o valor em dinheiro que o cliente forneceu, para então calcular e mostrar o valor do troco.
-# Após esta operação, o programa deverá voltar ao ponto inicial, para registrar a próxima compra. A saída deve ser conforme o exemplo abaixo:
-# Lojas Tabajara
-# Produto 1: R$ 2.20
-# Produto 2: R$ 5.80
-# Produto 3: R$ 0
-# Total: R$ 9.00
-# Dinheiro: R$ 20.00
-# Troco: R$ 11.00
+# Criar um algoritmo em PORTUGOL que leia dez números inteiros e
+#  imprima o maior
+# e o segundo maior número da lista.
 #
 # --------------------------------------------------------------------------
-# primeiro while para controlar o registro das compras
-n=0
-maior=0
-menor=9999
-while n<3:
-    num=int(input('numero'))
-    if num>maior:
-        maior=num
-    if num<menor:
-        menor=num
+
+n = 0
+maior = 0
+maior2 = 0
+while n < 3:
+    num = int(input('numero'))
+    if num > maior:
+        maior2=maior
+        maior = num
     n += 1
-print(maior,menor)
+print(maior, maior2)
