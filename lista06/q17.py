d0c466899f18a9dbf93258148ce8380a5dce9c51
VitorSimoes <vitor6427126@gmail.com>
2017-05-01 14:04:47 -0400

    remodelando

--- lista06/q17.py
+++ lista06/q17.py
@@ -26,21 +26,21 @@
 # Saltos: 6.5 - 6.1 - 6.2 - 5.4 - 5.3
 # Média dos saltos: 5.9 m
 list_saltos = []
-nome =[]
+list_nome=[]
 media = 0
-
-
-# while True:
-#     nome = str(input('Atleta: \b'))
-#     if (nome != ""):
-#         for i in range(5):
-#             list_saltos.append(float(input(str(i + 1) + ' Salto: \b')))
-#             media += list_saltos[i]
-#     else:
-#         media = media / 5
-#         print('Resultado Final:')
-#         for j in range(len(nome)):
-#             print('Atleta: ' + nome )
-#             print('Saltos: ' + str(list_saltos[j]) + ' - ' + str(list_saltos[j + 1]) + ' - ' + str(list_saltos[j + 2]) + ' - ' + str(list_saltos[j + 3]))
-#             print('Média dos Saltos: ' + str(media) + ' m')
-#
+j=0
+while True:
+    list_nome.append(str(input('Atleta: \b')))
+    if (list_nome[j] == ""):
+        break
+    else:
+        for i in range(5):
+            list_saltos.append(float(input(str(i + 1) + ' Salto: \b')))
+            media += list_saltos[i]
+        media = media / 5
+        print('Resultado Final:')
+        for i in range(len(list_nome)):
+            print('Atleta: ' + list_nome[i])
+            print('Saltos: ' + str(list_saltos[i]) + ' - ' + str(list_saltos[i + 1]) + ' - ' + str(list_saltos[i + 2]) + ' - ' + str(list_saltos[i + 3]))
+            print('Média dos Saltos: ' + str(media) + ' m')
+    j+=1
