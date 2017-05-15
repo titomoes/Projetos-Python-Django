7d6363e52b615e05af3cc86ee026f1802ef1d8ed
tiagoweb <tiagoweb@gmail.com>
2017-05-15 19:41:47 -0400

    up

---
+++ lista07/lista07_lista01_questao01.py
@@ -0,0 +1,16 @@
+# Criar um algoritmo que leia os elementos
+# de uma matriz inteira 10 x 10 e escreva os
+# elementos da diagonal principal.
+
+matriz = []
+# gerando matriz
+for i in range (3):
+    lista=[]
+    for j in range(3):
+        lista.append(input())
+    matriz.append(lista)
+
+for i,n in enumerate(matriz):
+    for j,m in enumerate(n):
+        if i==j:
+            print(m, end=' ')
