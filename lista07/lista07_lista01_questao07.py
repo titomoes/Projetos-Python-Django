acf1a3f6bceab3ddf697bc8d5c76017053aab531
tiagoweb <tiagoweb@gmail.com>
2017-05-15 20:00:34 -0400

    up

---
+++ lista07/lista07_lista01_questao07.py
@@ -0,0 +1,20 @@
+# Criar um algoritmo que leia os elementos de uma matriz inteira 10 x 10 e escreva os
+# elementos da diagonal secundária.
+
+matriz = []
+linhas = 4
+colunas = 4
+variavel = linhas + 1
+
+# gerando matriz
+for i in range (linhas):
+    lista=[]
+    for j in range(colunas):
+        lista.append(input())
+    matriz.append(lista)
+
+for i,n in enumerate(matriz):
+    for j,m in enumerate(n):
+        # ajuste do índice da matriz, que começa em 1 e não em 0
+        if j+i+2 == variavel:
+            print(m,end=' ')
