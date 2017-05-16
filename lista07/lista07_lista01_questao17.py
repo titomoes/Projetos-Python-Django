db3110008288bc07cc4946423438a65c0146f679
tiagoweb <tiagoweb@gmail.com>
2017-05-15 21:39:35 -0400

    up

--- lista07/lista07_lista01_questao17.py
+++ lista07/lista07_lista01_questao17.py
@@ -1,30 +1,36 @@
-# Uma floricultura conhecedora de sua clientela gostaria de fazer um algoritmo que
-# pudesse controlar sempre um estoque mínimo de determinadas plantas, pois todo dias,
-# pela manhã, o dono faz novas aquisições.
+# A gerente do cabeleireiro Sempre Bela tem uma tabela em que registra os “pés” as
+# “mãos” e o serviço de podologia das cinco manicures.
 #
-# Criar um algoritmo que deixe cadastrar 50
-# tipos de plantas e nunca deixar o estoque ficar abaixo do ideal. Para cada planta, o
-# dono gostaria de cadastrar o nome, o estoque ideal e a quantidade em estoque.
+# Sabendo-se que cada uma ganha 50% do que faturou ao mês,
 #
-# Dessa forma o algoritmo pode calcular a quantidade que o dono da loja precisa comprar no
-# próximo dia. Essa quantidade a ser comprada deve ser impressa (quando maior que
-# zero) como uma lista para o dono da floricultura.
+# criar um algoritmo que possa calcular e imprimir
+#
+# - quanto cada um vai receber, uma vez que não têm carteiras assinadas;
+#
+# os valores, respectivamente, são R$ 10,00; R$ 15,00 e R$ 30,00.
 
-estoque = []
-matriz_requisito = []
-linhas = 3
-colunas = 50
+funcionarios_salarios = []
+total_salarios = []
 
-for i in range(colunas):
-    planta = input("Informe no nome, o estoque ideal e a quantidade: Ex.: Flor 50 30 ").split()
-    planta_registro = [planta[0], int(planta[1]), int(planta[2])]
-    estoque.append(planta_registro)
+print(float(input()))
 
-for n in estoque:
-    lista_requisito = []
+qtd = 5
 
-    if n[1] > n[2]:
-        lista_requisito = [n[0], n[1] - n[2]]
-        matriz_requisito.append(lista_requisito)
+for i in range(qtd):
+    salario_registra = []
+    salario = input("Informe no nome, a qtd de pés, mãos e serviços de podologia: : Ex.: Maria 50 30 10 ").split()
 
-print(matriz_requisito)
+    salario_registra.append(float(salario[1]) * 10)
+    salario_registra.append(float(salario[2]) * 15)
+    salario_registra.append(float(salario[3]) * 30)
+    print(salario_registra)
+    funcionarios_salarios.append(salario)
+
+print(funcionarios_salarios)
+
+for n in funcionarios_salarios:
+    salario = []
+    salario = [n[0], (n[1] + n[2] + n[3]) * 0.5]
+    total_salarios.append(salario)
+
+print(total_salarios)
